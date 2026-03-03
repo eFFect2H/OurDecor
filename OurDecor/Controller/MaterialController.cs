0abbf17ce233a43bddeefa524465fb5733de6d58
eFFect2H <matvey.shumovskiy@bk.ru>
2026-03-04 00:42:51 +0500

    Добавл документацию с диагарммой-БД и Блок-схуму приложения. Перехожу к созданию интерфейса для управления над данными

--- OurDecor/Controller/MaterialController.cs
+++ OurDecor/Controller/MaterialController.cs
@@ -2,7 +2,6 @@
 using OurDecor.Models;
 using System.Threading.Tasks;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace OurDecor.Controller
 {
